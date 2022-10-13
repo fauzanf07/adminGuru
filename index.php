92bc4cde0fef053ed91c980c1c1679a0c3ac1f24
unknown <wush3222@gmail.com>
2022-10-13 10:54:58 +0700

    add content

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <!DOCTYPE html>
 <html>
+
 <head>
 	<title>SmartTeach</title>
 	<link rel="stylesheet" type="text/css" href="style/landing-page/style.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 </head>
+
 <body>
 	<div class="nav-parent">
 		<nav class="navbar navbar-expand-lg sticky-top">
@@ -48,7 +50,29 @@
 		<h2>Menjadikan Pekerjaan Guru Lebih Mudah</h2>
 		<a href="#" class=" btn-contact" role="button" data-bs-toggle="button">CONTACT US</a>
 	</div>
+
+	<div class="content">
+		<div class="row-1">
+			<div class="container">
+			  	<div class="row">
+				    <div class="col">
+				     	<img src="https://static.wixstatic.com/media/4c1c57_fc8e75822abf45fea52c68746752d473~mv2.png/v1/crop/x_516,y_0,w_887,h_758/fill/w_550,h_470,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/62d12fd15647b.png" alt="Books" width="550px" height="470px">
+				    </div>
+				    <div class="col font-col">
+				     	<h2 class="the-nineteen">THE NINETEEN</h2>
+				     	<h3 class="jargon">Providing solutions for Indonesian education</h3>
+				     	<p class="quotes">“Pendidikan adalah senjata paling<br>mematikan di dunia, karena dengan pendidikan,<br>kamu dapat mengubah dunia” – Nelson Mandela</p>
+				    </div>
+			  	</div>
+			</div>
+		</div>
+
+		<div class="row-2">
+			<h2>HOW DID IT WORKS</h2>
+		</div>
+	</div>
 </body>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
