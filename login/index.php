f093930d77bd75c3cae89027ebaeecf4eaa9281b
unknown <wush3222@gmail.com>
2022-10-14 23:09:49 +0700

    add login page

---
+++ login/index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>SmartTeach</title>
+	<link rel="stylesheet" type="text/css" href="../style/login/style.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+</head>
+
+<body>
+    <div class="nav-parent">
+		<nav class="navbar navbar-expand-lg navbar-light" style="background-color: #FFFFFF;">
+			<div class="pink-on-top"></div>
+			<div class="container-fluid">
+				<div class="logo-parent">
+					<div class="logo-icon"></div>
+					<div class="logo-text">
+						<h4 class="logo-name">THE NINETEEN</h4>
+						<a href="#" class="desc"><h4 class="logo-name desc">Providing solutions for Indonesian education</h4></div></a>
+					</div>
+				</div>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Online Course</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">About Us</a>
+					</li>
+				</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+    <div class="content">
+		<div class="row-1">
+			<div class="container">
+				<div class="row">
+					<div class="col"></div>
+					<div class="col">
+						<div class="border">
+							<form>
+								<div class="form-group">
+									<label for="exampleInputEmail1">Email address</label>
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+									<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+								</div>
+								<div class="form-group">
+									<label for="exampleInputPassword1">Password <span class="forgot-password"><a href="#">Forgot password?</a></span></label>
+									<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+								</div>
+								<div class="login-button">
+									<div class="d-grid gap-2">
+										<button class="btn btn-primary btn btn-success" type="button">Log in</button>
+									</div>
+								</div>
+							</form>
+						</div>
+
+						<div class="border">
+							<div class="signup-direction">
+								<p>New to adminGuru? <span><a href="#">Create an account</a></span></p>
+							</div>
+						</div>
+
+					</div>
+					<div class="col">
+					</div>
+				</div>
+			</div>
+		</div>
+    </div>
+
+	<div class="footer">
+		<div class="copyright">
+			<p>© 2022 by The Nineteen</p>
+		</div>
+		<div class="background-footer"></div>
+	</div>
+    
+</body>
+
+</html>
