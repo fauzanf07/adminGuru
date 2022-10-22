f6bc6234285f8b3a6258db64d8a479cb36f8fa07
unknown <wush3222@gmail.com>
2022-10-22 17:13:52 +0700

    change item in navbar

--- login/index.php
+++ login/index.php
@@ -10,41 +10,40 @@
 </head>
 
 <body>
-	<div class="wrapper">
-		<div class="nav-parent">
-			<nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #FFFFFF;">
-				<div class="pink-on-top"></div>
-				<div class="container-fluid">
-					<div class="logo-parent">
-						<div class="logo">
-							<img class="logo-icon" src="../images/logo-removebg-preview.png" alt="logo">
-						</div>
+	<div class="nav-parent">
+		<nav class="navbar navbar-expand-lg sticky-top navbar-dark" style="background-color: #FFFFFF;">
+			<div class="pink-on-top"></div>
+			<div class="container-fluid">
+				<div class="logo-parent">
+					<div class="logo">
+						<img class="logo-icon" src="../images/logo-removebg-preview.png" alt="logo">
 					</div>
-					<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-					</button>
-					<div class="collapse navbar-collapse" id="navbarNav"  >
-						<ul class="navbar-nav mb-2 mb-lg-0">
-					        <li class="nav-text" >
-					          <a class="nav-link" aria-current="page" href="blog">Blog</a>
-					        </li>
-					        <li class="nav-text">
-					          <a class="nav-link active" href="../backend/logout/logout.php"><i class="bi bi-person-circle"></i>&nbsp;&nbsp;Log Out</a>
-					        </li>
-					        <li class="nav-icon">
-					          <a class="nav-link" href="#"><i class="bi bi-facebook"></i></a>
-					        </li>
-					        <li class="nav-icon">
-					          <a class="nav-link" href="#"><i class="bi bi-twitter"></i></a>
-					        </li>
-					        <li class="nav-icon">
-					          <a class="nav-link" href="#"><i class="bi bi-youtube"></i></a>
-					        </li>
-						</ul>
-				    </div>
 				</div>
-			</nav>
-		</div>
+				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarNav"  >
+					<ul class="navbar-nav mb-2 mb-lg-0">
+						<li class="nav-text" >
+							<a class="nav-link" aria-current="page" href="blog">Blog</a>
+						</li>
+						<li class="nav-text">
+							<a class="nav-link active" href="../backend/logout/logout.php"><i class="bi bi-person-circle"></i>&nbsp;&nbsp;Log Out</a>
+						</li>
+						<li class="nav-icon">
+							<a class="nav-link" href="#"><i class="bi bi-facebook"></i></a>
+						</li>
+						<li class="nav-icon">
+							<a class="nav-link" href="#"><i class="bi bi-twitter"></i></a>
+						</li>
+						<li class="nav-icon">
+							<a class="nav-link" href="#"><i class="bi bi-youtube"></i></a>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</nav>
+	</div>
 
     <div class="content">
 		<div class="row-1">
