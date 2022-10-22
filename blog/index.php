ad5ab5fc20acb357c524bd8e89dde79bca75b49a
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2022-10-23 00:50:17 +0700

    blog menu

--- blog/index.php
+++ blog/index.php
@@ -31,7 +31,7 @@
 				<div class="collapse navbar-collapse" id="navbarNav"  >
 					<ul class="navbar-nav mb-2 mb-lg-0">
 						<li class="nav-text" >
-							<a class="nav-link" aria-current="page" href="blog">Blog</a>
+							<a class="nav-link" aria-current="page" href="../">Home</a>
 						</li>
 						<li class="nav-text">
 							<a class="nav-link active" href="../backend/logout/logout.php"><i class="bi bi-person-circle"></i>&nbsp;&nbsp;Log Out</a>
