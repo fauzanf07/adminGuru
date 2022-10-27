0ca1a31f30f89d65a63fa8913003b425c5d51493
unknown <wush3222@gmail.com>
2022-10-27 11:26:01 +0700

    add icon and change title

--- login/index.php
+++ login/index.php
@@ -9,8 +9,9 @@
 <html>
 
 <head>
-	<title>SmartTeach</title>
+	<title>adminguru &centerdot; Login</title>
 	<link rel="stylesheet" type="text/css" href="../style/login/style.css">
+	<link rel="icon" type="image/x-icon" href="../images/logo.ico">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
