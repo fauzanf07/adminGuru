facd79293c288a2e68fd42edc7c6bc9c4a60b195
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2022-11-24 23:03:37 +0700

    fixed redirect url in edit-profile

--- edit-profile/edit.php
+++ edit-profile/edit.php
@@ -1,7 +1,7 @@
 <?php 
 	session_start();
 	if(!isset($_SESSION['nama'])){
-		header("Location: http://localhost/adminGuru/login");
+		header("Location: ../login");
 	}
 	include("../backend/conn.php");
 
