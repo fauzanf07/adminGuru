5c50fa72edf07152149223b3e6e97f7f03b198e6
unknown <wush3222@gmail.com>
2022-12-27 11:45:37 +0700

    make a responsive pricing page

--- pricing/index.php
+++ pricing/index.php
@@ -12,7 +12,7 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap5.min.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="viewport" content="width=device-width, initial-scale=0.4">
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 </head>
 
@@ -229,7 +229,7 @@
 						</div>
 					</div>
 				</div>
-				<div class="col-12">
+				<div class="col-12 col-history">
 					<div class="card bord-4">
 						<div class="title-card">
 							<i class="bi bi-credit-card-2-back-fill"></i>&nbsp;&nbsp;Pembayaran Anda
