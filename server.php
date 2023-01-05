807686fd0904a09ff01cca897540eb8bf2fef3da
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2023-01-05 21:48:35 +0700

    update conn and server

--- server.php
+++ server.php
@@ -1,3 +1,13 @@
 <?php
-    $mainUrl = "https://localhost/adminguru/"; 
+    $whitelist = array(
+        '127.0.0.1',
+        '::1'
+    );
+
+    if(in_array($_SERVER['REMOTE_ADDR'], $whitelist)){
+        $mainUrl = "https://localhost/adminguru/"; 
+    }else{
+        $mainUrl = "https://adminguru.id/"; 
+    }
+    
 ?>
