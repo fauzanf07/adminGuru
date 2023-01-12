9702c26699c3eb3bc81ab69297371c63b265e0d4
Fauzan Fiqriansyah <fauzanfiqriansyah126@gmail.com>
2023-01-12 20:45:42 +0700

    changing api secret pdf

--- backend/logout/logout.php
+++ backend/logout/logout.php
@@ -1,3 +1,16 @@
+<script>
+const cookies = document.cookie.split(";");
+
+for (let i = 0; i < cookies.length; i++) {
+	const cookie = cookies[i];
+	const sCookie = cookie.split("=");
+	if(sCookie[0]!="PHPSESSID"){
+		var name = String(sCookie[0]);
+		Cookies.remove(name.replace(/\s/g, ""));
+	}
+}
+$('#formModul').sayt({'erase': true});
+</script>
 <?php
 	require_once '../../vendor/autoload.php';
 	session_start();
